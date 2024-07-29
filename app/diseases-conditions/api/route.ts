f422be0989278efa58da018248f1f72347cba34b
Murad <yusifovmurad1@gmail.com>
2024-07-29 13:05:59 +0400

    Display diseases #3

--- app/diseases-conditions/api/route.ts
+++ app/diseases-conditions/api/route.ts
@@ -1,65 +1,75 @@
 import puppeteer from "puppeteer";
-const allLetters = [
-  "A",
-  "B",
-];
+const allLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+type Disease = {
+  name: string;
+  url: string | null;
+  seeMore: {
+    text: string | null | undefined;
+    url: string | null | undefined;
+  };
+};
+
 export async function GET(request: Request) {
   let data;
   const training = async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto("https://www.mayoclinic.org/diseases-conditions");
 
-    const diseaseNames = {};
+    const diseaseNames: { [key: string]: Disease[] } = {};
 
     for (const letter of allLetters) {
       await page.goto(
         `https://www.mayoclinic.org/diseases-conditions/index?letter=${letter}`
       );
-      const diseases = (
-        await page.$$(`#cmp-skip-to-main__content  .cmp-result-letters + ul li`)
-      ).filter((_, idx) => idx < 10);
+      const diseases = (await page.$$(`.cmp-result-letters + ul li`)).filter(
+        (_, idx) => idx < 10
+      );
 
-      const diseaseArr = [];
+      const diseaseArr: Disease[] = [];
       await Promise.all(
         diseases.map(async (disease) => {
-          const diseaseName = await disease.evaluate((el) => {
+          const diseaseName = await disease.evaluate((el: Element) => {
             return (
-              el.querySelector(".cmp-result-name a")?.textContent ||
-              el.querySelector(".cmp-results-with-primary-name p")?.textContent
+              el.querySelector<HTMLElement>(".cmp-result-name a")
+                ?.textContent ||
+              el.querySelector<HTMLElement>(".cmp-results-with-primary-name p")
+                ?.textContent ||
+              "N/A"
             );
           });
 
-          const diseaseUrl = await disease.evaluate((el) => {
-            return el.querySelector(".cmp-result-name a")?.href || "N/A";
+          const diseaseUrl = await disease.evaluate((el: Element) => {
+            return (
+              el.querySelector<HTMLAnchorElement>(".cmp-result-name a")?.href ||
+              "N/A"
+            );
           });
 
-          const seeMore = await disease.evaluate((el) => {
-            const seeMoreElement = el.querySelector(
+          const seeMore = await disease.evaluate((el: Element) => {
+            const seeMoreElement = el.querySelector<HTMLElement>(
               ".cmp-results-with-primary-name__see"
             );
 
             return {
               text: seeMoreElement ? seeMoreElement?.textContent : "N/A",
               url: seeMoreElement
-                ? seeMoreElement?.querySelector("a")?.href
+                ? seeMoreElement.querySelector<HTMLAnchorElement>("a")?.href
                 : "N/A",
             };
           });
 
-          const diseaseObj = {
-            name: diseaseName,
+          const diseaseObj: Disease = {
+            name: diseaseName || "N/A",
             url: diseaseUrl,
             seeMore,
           };
           diseaseArr.push(diseaseObj);
 
-          // await setDoc(doc(db, "diseases", letter), diseaseArr, {
-          //   merge: true,
-          // });
+
         })
       );
-      // await setDoc(doc(db, "diseases", letter), diseaseArr, { merge: true });
       diseaseNames[letter] = diseaseArr;
     }
 
--- app/diseases-conditions/page.tsx
+++ app/diseases-conditions/page.tsx
@@ -1,11 +1,19 @@
 "use client";
-import { collection, getDocs, doc, setDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/firebase";
 import { useEffect, useState } from "react";
 import { DiseaseConditionItem } from "@/components/index";
-import puppetter from "puppeteer";
+import { Button } from "@/components/ui/button";
+type DiseasesConditionsItem = {
+  name: string;
+  url: string;
+  seeMore: {
+    text: string;
+    url: string;
+  };
+};
 type DiseasesConditions = {
-  [key: string]: string[];
+  [key: string]: DiseasesConditionsItem[];
 };
 
 const DiseasesConditions = () => {
@@ -13,32 +21,58 @@ const DiseasesConditions = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  // const fetchDiseasesConditions = async () => {
-  //   try {
-  //     const querySnapshot = await getDocs(collection(db, "diseases"));
-  //     const fetchedDiseases: DiseasesConditions = {};
-  //     querySnapshot.forEach((doc) => {
-  //       const data = doc.data() as DiseasesConditions;
-
-  //       const sortedKeys = Object.keys(data).sort();
-
-  //       for (const letter of sortedKeys) {
-  //         fetchedDiseases[letter] = data[letter];
-  //       }
-  //     });
-  //     setDiseases(fetchedDiseases);
-  //   } catch (error) {
-  //     console.error("Error fetching diseases:", error);
-  //     setError("An error occurred while fetching the diseases."); // Set error message
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
   useEffect(() => {
-    // fetchDiseasesConditions();
+    const fetchDiseasesConditions = async () => {
+      try {
+        const diseasesRef = collection(db, "diseases");
+        const docSnap = await getDocs(diseasesRef);
+        const diseasesConditions: DiseasesConditions = {};
+        if (docSnap) {
+          docSnap.forEach((doc) => {
+            const data = doc.data();
+            const docId = doc.id;
+            diseasesConditions[docId] = data.conditions;
+          });
+        }
+        setDiseases(diseasesConditions);
+      } catch (error) {
+        console.error("Error fetching diseases:", error);
+        setError("An error occurred while fetching the diseases."); // Set error message
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchDiseasesConditions();
   }, []);
 
+  // useEffect(() => {
+  //   const fetchDiseasesConditions = async () => {
+  //     try {
+  //       const response = await fetch("/diseases-conditions/api");
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       const data: DiseasesConditions = await response.json();
+  //       if (data) {
+  //         Object.entries(data).forEach(([letter, conditions]) => {
+  //           try {
+  //             setDoc(doc(db, "diseases", letter), {conditions}, { merge: true });
+  //           } catch (err) {
+  //             console.log(err);
+  //           }
+  //         });
+  //       }
+  //       setDiseases(data);
+  //     } catch (error) {
+  //       console.error("Error fetching diseases:", error);
+  //       setError("An error occurred while fetching the diseases.");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   fetchDiseasesConditions();
+  // }, []);
   return (
     <main className="px-4 py-8 sm:px-16 md:px-32">
       <div className="mx-auto max-w-7xl flex flex-col gap-6">
@@ -47,14 +81,29 @@ const DiseasesConditions = () => {
         </h1>
 
         {loading ? (
-          <div className="flex justify-center items-center"></div>
+          <div className="flex justify-center items-center">LOADING...</div>
         ) : error ? (
           <p className="text-center text-red-500">{error}</p>
         ) : (
           Object.entries(diseases).map(([letter, conditions]) => (
             <DiseaseConditionItem key={letter} mainLetter={letter}>
               {conditions.map((condition) => (
-                <li key={condition}>{condition}</li>
+                <li key={condition.name}>
+                  <a
+                    target="_blank"
+                    href={
+                      condition.url !== "N/A"
+                        ? condition.url
+                        : condition.seeMore.url
+                    }
+                  >
+                    <Button className="whitespace-break-spaces text-start" variant="link">
+                      {condition.seeMore.text === "N/A"
+                        ? condition.name
+                        : `${condition.name} - ${condition.seeMore.text}`}
+                    </Button>
+                  </a>
+                </li>
               ))}
             </DiseaseConditionItem>
           ))
